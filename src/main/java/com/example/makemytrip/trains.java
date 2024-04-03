4b0593c6af33b12a9e37f81645865fd4127258eb
Meena Kasbekar <mkasbekar23@gmail.com>
2024-04-03 23:50:29 +0530

    added new train class

---
+++ src/main/java/com/example/makemytrip/trains.java
@@ -0,0 +1,11 @@
+package com.example.makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class trains {
+    @GetMapping("/trains")
+    public String getData() {return "TICKETS AVAILABLE";}
+
+}
