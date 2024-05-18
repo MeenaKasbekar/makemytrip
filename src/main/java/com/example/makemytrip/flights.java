e8618c7bb365f957a7cad75c0d32cfeb61c96c58
Meena Kasbekar <mkasbekar23@gmail.com>
2024-05-18 23:41:04 +0530

    modified flights class

--- src/main/java/com/example/makemytrip/flights.java
+++ src/main/java/com/example/makemytrip/flights.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class flights {
     @GetMapping("/flights")
-    public String getData() {return "20% DISCOUNT ON FLIGHTS BOOKING FROM AIR INDIA";}
+    public String getData() {return "20% DISCOUNT ON FLIGHTS BOOKING FROM AIR INDIA AND INDIGO";}
 
 }
 
